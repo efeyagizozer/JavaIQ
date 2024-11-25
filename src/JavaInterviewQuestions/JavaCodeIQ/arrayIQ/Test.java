26da9d6114a22dc8ad522662c68ae036771e1afb
efeyagizozer <ozerefe2002@gmail.com>
2024-11-25 09:26:41 -0500

    For loop added

--- src/JavaInterviewQuestions/JavaCodeIQ/arrayIQ/Test.java
+++ src/JavaInterviewQuestions/JavaCodeIQ/arrayIQ/Test.java
@@ -2,6 +2,19 @@ package JavaInterviewQuestions.JavaCodeIQ.arrayIQ;
 
 public class Test {
 
+    // note
+    // deneme
+
+    // github
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("good try ");
+        }
+
+    }
+
 
 
 
