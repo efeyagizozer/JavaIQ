aaa709dbd9608d396b6ffc12788255baf11c3bed
TalhaDoganGit <talha.emir.dogan@gmail.com>
2024-11-27 10:56:47 -0500

    Efe Emir Deneme

---
+++ src/Java/Ifs/String_Manipulation.java
@@ -0,0 +1,16 @@
+package Java.Ifs;
+
+public class String_Manipulation {
+    public static void main(String[] args) {
+        String string =  "Wooden Spoon";
+        char ch=string.charAt(5);
+
+        System.out.println(ch);
+
+       int index1= string.indexOf("o");
+
+
+        System.out.println(index1);
+
+    }
+}
